4c25062e4dfe3d7c99c91fc42231d0169e164306
Matthieu Queru <75125755+Matthieu33197@users.noreply.github.com>
2024-07-17 00:22:13 +0900

    [ADD] création du modèle CNN et mise en place des couches convolutionnelles

--- cardReader.py
+++ cardReader.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import tensorflow as tf
+import cv2
+import pyautogui
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from keras.src.models import Sequential
 from keras.src.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
@@ -37,4 +39,31 @@ test_generator = test_datagen.flow_from_directory(
     target_size=(IMG_SIZE, IMG_SIZE),
     batch_size=BATCH_SIZE,
     class_mode='categorical'
+)
+
+# Création du modèle CNN
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dropout(0.5),
+    Dense(train_generator.num_classes, activation='softmax')
+])
+
+# Compilation du modèle
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entraînement du modèle
+history = model.fit(
+    train_generator,
+    epochs=10,
+    validation_data=valid_generator
 )
